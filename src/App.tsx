d8ed10c8d4f7e29fde63cc29fd1fc459d5409705
Ilya Sharonov <sh@ronov.email>
2023-02-23 00:24:37 +0400

    Working sample for IndexDB test on Android

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,53 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import localforage from 'localforage';
 import './App.css';
 
+function createBlob(size = 1024 * 1024, fill = 255) {
+    return new Uint8Array(size).fill(fill);
+}
+
 function App() {
+    const stored = useRef<any>();
+    const [status, setStatus] = useState('');
+    const writing = useRef<boolean>(false);
+    async function startWrite() {
+        writing.current = true;
+        setStatus('Writing');
+        localforage.setDriver(localforage.INDEXEDDB);
+        const blobSize = 100 * 1024 * 1024;
+        let iteration = 0;
+        try {
+            while (writing.current) {
+                iteration++;
+                const blob = createBlob(blobSize);
+                await localforage.setItem(String(iteration), blob);
+                if (stored.current) {
+                    stored.current.textContent = `${Math.floor((blobSize * iteration) / (1024 * 1024))} Mb`;
+                }
+            }
+        } catch (e) {
+            console.log(e);
+            writing.current = false;
+        }
+    }
+    function stopWrite() {
+        setStatus('Halt');
+        writing.current = false;
+    }
+    async function clearStorage() {
+        await localforage.clear();
+        setStatus('Storage cleared');
+    }
     return (
         <div className="App">
-            <form action="/foto" method="post" encType="multipart/form-data">
-                <input accept="image/*" id="icon-button-file" name="foto" type="file" capture="environment" />
-                <input type="submit" value="Send" />
-            </form>
+            <button onClick={startWrite}>Start write</button>
+            <button onClick={stopWrite}>Stop write</button>
+            <button onClick={clearStorage}>Clear storage</button>
+            <div>
+                <span>Written to storage: </span>
+                <span id="stored" ref={stored}></span>
+            </div>
+            <div id="status">Status: {status}</div>
         </div>
     );
 }
